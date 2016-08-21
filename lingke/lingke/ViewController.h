252ebdcfb70d14ac15bf84239b06a9241c3cfac3
cuilizhong <Cui_Li_Zhong@163.com>
2016-08-21 09:37:13 +0800

    basic

---
+++ lingke/lingke/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lingke
+//
+//  Created by clz on 16/8/21.
+//  Copyright © 2016年 CLZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lingke/lingke/ViewController.m
@@ -0,0 +1,25 @@
+//
+//  ViewController.m
+//  lingke
+//
+//  Created by clz on 16/8/21.
+//  Copyright © 2016年 CLZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+@end
