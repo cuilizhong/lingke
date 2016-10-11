c5ac8fd8e01d6c9a1cd6f5ec931485c6cd718564
cuilizhong <Cui_Li_Zhong@163.com>
2016-10-11 09:56:32 +0800

    修复部分附件无法查看问题

--- lingke/lingke/PreviewFileViewController.m
+++ lingke/lingke/PreviewFileViewController.m
@@ -22,27 +22,31 @@
     
     self.title = @"查看附件";
     
-    self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height-64)];
     [self.view addSubview:self.webView];
     
     NSURL *url = [NSURL fileURLWithPath:self.filepath];
     
     NSData *data = [NSData dataWithContentsOfFile:self.filepath];
     
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    NSURLResponse *response = nil;
-//    [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:nil];
-//    NSString *MIMEType = [response MIMEType];
     
-    
-    
-//    [self.webView loadData:data MIMEType:[NSString stringWithFormat:@"application/%@",self.filepath.pathExtension] textEncodingName:@"UTF-8" baseURL:url];
-    
-    
-    [self.webView loadData:data MIMEType:[NSString stringWithFormat:@"application/%@",self.filepath.pathExtension] textEncodingName:@"UTF-8" baseURL:url];
+    if([self.filepath.pathExtension rangeOfString:@"txt"].location !=NSNotFound){
+        
+        [self.webView loadData:data MIMEType:@"text/plain" textEncodingName:@"UTF-8" baseURL:nil];
+        
+    }else if([self.filepath.pathExtension rangeOfString:@"gif"].location !=NSNotFound){
+        
+        [self.webView loadData:data MIMEType:@"image/gif" textEncodingName:nil baseURL:nil];
+        
+    }else{
+        
+        [self.webView loadData:data MIMEType:[NSString stringWithFormat:@"application/%@",self.filepath.pathExtension] textEncodingName:@"UTF-8" baseURL:url];
+
+    }
+
+    [self.webView scalesPageToFit];
     
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
